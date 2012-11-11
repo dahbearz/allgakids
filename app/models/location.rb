e148e7018069463451549c195df986f97614fbb0
Peter <parkper@gmail.com>
2012-11-11 06:57:31 -0500

    Geolocation

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,13 @@
 class Location < ActiveRecord::Base
   has_many :businesses
 
-  attr_accessible :address, :city, :state, :zipp, :county
+  attr_accessible :address, :city, :state, :zipp, :county, :lat, :long
+
+  geocoded_by :full_address, :latitude  => :lat, :longitude => :long
+  after_validation :geocode
+
+  def full_address
+    return self.address << "," << self.city << "," << self.state if address && city && state
+  end
 
 end
