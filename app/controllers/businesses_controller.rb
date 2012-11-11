984a8fced67f354ae1636d0630dfda640403fe69
Peter <parkper@gmail.com>
2012-11-11 10:16:32 -0500

    location functionality

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -3,7 +3,7 @@ class BusinessesController < ApplicationController
     @businesses = Business.scoped
     @businesses = @businesses.where("type_of_care IN (:type_of_care)", :type_of_care => params[:type_of_care]) if params[:type_of_care]
     @businesses = @businesses.where("max_age >= ? AND min_age <= ? ", params[:age],params[:age]) if params[:age]
-    @businesses = @businesses.where("location_id IS NOT NULL") & Location.near([params[:lat],params[:lon]], params[:range], :order => :distance)
+    @businesses = @business.where("location_id IN (:location)", :location => Location.near([33.7459347,-84.220213],100,:order => :distance))
 
     respond_to do |format|
       format.xml { render :xml => @businesses }
